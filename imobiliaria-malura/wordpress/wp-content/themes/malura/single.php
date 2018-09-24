5423d198274e62cfe0fe5dad7b82c4345fd054c5
Felipe <Feelipe@email.com>
2018-09-24 17:16:21 -0300

    Usamos um require_once para funcionando do CSS

--- imobiliaria-malura/wordpress/wp-content/themes/malura/single.php
+++ imobiliaria-malura/wordpress/wp-content/themes/malura/single.php
@@ -1,44 +1,48 @@
-<?php get_header(); ?>
+<?php
+$css_escolhido = 'single';
+require_once('header.php');
+?>
 
 <main>
+	
+	<article>
 
-    <article>
+		<?php if( have_posts() ) {
+			while( have_posts() ) {
+				the_post(); ?>
 
-        <?php 
-        if( have_posts() ) {
-            while( have_posts() ) {
-                the_post(); 
-        ?>
+		<div class="single-imovel-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div>
 
-        <div class="single-imovel-thumbnail">
-            <?php the_post_thumbnail(); ?>
-        </div>
+		<div class="container">
+			<section class="chamada-principal">
+				<h1><?php the_title(); ?></h1>
+			</section>
 
-        <div class="container">
-            <section class="chamada-principal">
-                <?php the_title(); ?>
-            </section>
+			<section class="single-imovel-geral">
+				
+				<div class="single-imovel-descricao">
+					<?php the_content(); ?>
+				</div>
 
-            <section class="single-imovel-geral">
-                <div class="single-imovel-descrição">
-                    <?php the_content(); ?>
-                </div>
-            </section>
 
-            <span class="single-imovel-data">
-              <?php the_date(); ?>
-            </span>
-              
-        </div>
+			</section>
 
-    <?php 
-        }
-    } 
+			<span class="single-imovel-data">
+			 <?php the_date(); ?>
+			</span>
 
-    ?>
 
-    </article>
+		</div>
+		
+		<?php }
+		} ?>
+
+	</article>
 
 </main>
+
+
 
 <?php get_footer(); ?>
