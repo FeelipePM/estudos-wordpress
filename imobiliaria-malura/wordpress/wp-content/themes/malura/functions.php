7d95dcda251b4cb4be41e817b44a3a1c7cf953ae
Felipe <Feelipe@email.com>
2018-09-24 17:13:25 -0300

    criado função para filtrar localizações

--- imobiliaria-malura/wordpress/wp-content/themes/malura/functions.php
+++ imobiliaria-malura/wordpress/wp-content/themes/malura/functions.php
@@ -34,3 +34,38 @@ register_post_type( 'imovel', $args );
 
 add_action('init', 'cadastrando_post_type_imoveis');
 
+function registrar_menu_navegacao() { 
+  register_nav_menu('header-menu', 'main-menu');
+}
+
+add_action('init', 'registrar_menu_navegacao');
+
+function geraTitle() {
+  bloginfo ('name'); 
+  if( !is_home() ) echo ' | ';
+  the_title();
+}
+
+function registra_taxonomia_localizacao() {
+
+$nomeSingular = 'Localização';
+$nomePlural = 'Localizações';
+
+$labels = array(
+  'name' => $nomePlural,
+  'singular_name' => $nomeSingular,
+  'edit_item' => 'Editar ' . $nomeSingular,
+  'add_new_item' => 'Adicionar nova '. $nomeSingular
+);
+
+$args = array(
+  'labels' => $labels,
+  'public' => true,
+  'hierarchical' => true
+);
+
+register_taxonomy('localizacao', 'imovel', $args);
+
+}
+
+add_action('init', 'registra_taxonomia_localizacao');
