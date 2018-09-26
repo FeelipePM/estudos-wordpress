d98d84a9eb6933d892628f389e332a2b178b34c2
Felipe <Feelipe@email.com>
2018-09-26 15:34:01 -0300

    configuramos o plugin SMTP Mailer

--- imobiliaria-malura/wordpress/wp-content/themes/malura/page.php
+++ imobiliaria-malura/wordpress/wp-content/themes/malura/page.php
@@ -1,51 +1,67 @@
 <?php
 $css_escolhido = 'page';
 require_once('header.php');
+
+$nome = $_POST['form-nome'];
+$email = $_POST['form-email'];
+$mensagem = $_POST['form-mensagem'];
+
+$formularioEnviado = isset($nome) && isset($email) && isset($mensagem);
+
+if($formularioEnviado) {
+    $enviou = enviar_e_checar_email($nome, $email, $mensagem);
+
+    if($enviou) { ?>
+        <span class="email-sucesso">Seu e-mail foi enviado com sucesso!</span>
+    <?php } else { ?>
+        <span class="email-fracasso">Desculpe, ocorreu um erro, seu e-mail não foi enviado!</span>
+    <?php } 
+}
 ?>
 
 <main class="pagina-main">
-	
-	<article class="pagina">
-		<h1 class="pagina-titulo">
-			<?php the_title(); ?>
-		</h1>
 
-		<?php if( have_posts() ) {
-			while( have_posts() ) {
-				the_post(); ?>
+    <article class="pagina">
+        <h1 class="pagina-titulo">
+            <?php the_title(); ?>
+        </h1>
 
-			<div class="pagina-conteudo">
-			 <?php the_content(); ?>
-			</div>
+        <?php if( have_posts() ) {
+            while( have_posts() ) {
+                the_post(); ?>
 
-		<?php	}
-		} 
+            <div class="pagina-conteudo">
+             <?php the_content(); ?>
+            </div>
 
-		if( is_page('contato') ) {
-		?>
+        <?php    }
+        } 
 
-		<form>
-			<div class="form-nome">
-				<label for="form-nome">Nome:</label>
-				<input id="form-nome" type="text" placeholder="Seu nome aqui" name="form-nome">
-			</div>
+        if( is_page('contato') ) {
+        ?>
 
-			<div class="form-email">
-				<label for="form-email">Email:</label>
-				<input id="form-email" type="email" placeholder="email@exemplo.com.br" name="form-email">
-			</div>
-		
-			<div class="form-mensagem">
-				<label for="form-mensagem">Mensagem:</label>
-				<textarea id="form-mensagem" name="form-mensagem"></textarea>
-			</div>
-			<button type="submit">Enviar</button>
+        <form method="post">
+            <div class="form-nome">
+                <label for="form-nome">Nome:</label>
+                <input id="form-nome" type="text" placeholder="Seu nome aqui" name="form-nome">
+            </div>
 
-		</form>
+            <div class="form-email">
+                <label for="form-email">Email:</label>
+                <input id="form-email" type="email" placeholder="email@exemplo.com.br" name="form-email">
+            </div>
 
-	<?php } ?>
+            <div class="form-mensagem">
+                <label for="form-mensagem">Mensagem:</label>
+                <textarea id="form-mensagem" name="form-mensagem"></textarea>
+            </div>
+            <button type="submit">Enviar</button>
 
-	</article>
+        </form>
+
+    <?php } ?>
+
+    </article>
 </main>
 
 <?php get_footer(); ?>
